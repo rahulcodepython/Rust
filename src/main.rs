01cb4c8ad072200648651a141b78c68fbc5e625d
rahulcodepython <rahulcodepython@gmail.com>
2024-10-17 14:00:32 +0000

    ADD: Condition

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,13 @@
 fn main() {
-    let guess: u32 = "42".parse().expect("Not a number!");
-    println!("Guess: {}", guess);
+    let number = 6;
 
-    let x: i32 = 5; // integer
-    let y: f32 = 3.0; // floating number
-    let z: bool = true; // boolean
-    let a: char = 'a'; // character
-    let b: [i32; 5] = [1, 2, 3, 4, 5]; // array - [type; size]
-    let c: (i32, f64, u8) = (500, 6.4, 1); // tuple
-    let (d, e, f) = c; // destructuring tuple
-    let g = c.0; // access tuple element
-    let h = c.1; // access tuple element
-
-    let months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-    println!("Months: {:?}", months);
-
-    println!("x: {}", x);
-    println!("y: {}", y);
-    println!("z: {}", z);
-    println!("a: {}", a);
-    println!("b: {:?}", b);
-    println!("c: {:?}", c);
-    println!("d: {}", d);
-    println!("e: {}", e);
-    println!("f: {}", f);
-    println!("g: {}", g);
-    println!("h: {}", h);
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
