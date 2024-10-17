53dd3f08792042a504e6306e8c130f30986e37ef
rahulcodepython <rahulcodepython@gmail.com>
2024-10-17 13:39:20 +0000

    ADD: Variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5; // mutable variable
+    let y = 8; // immutable variable
+
+    println!("The value of x is: {x}"); // before updating x
+    x = 6;
+    println!("The value of x is: {x}"); // after updating x
+
+    println!("The value of y is: {y}");
+
+    {
+        let x = x * 2; // shadowing && local scope
+        println!("The value of x in the inner scope is: {x}"); // access the inner scope
+    }
+
+    println!("The value of x is: {x}"); // access the outer scope
+
+    let x = x * 2; // shadowing
+    println!("The value of x is: {x}");
 }
