d5e578d9c2cb2a523ab20b894b45328d7c36024d
rahulcodepython <rahulcodepython@gmail.com>
2024-10-17 13:46:14 +0000

    ADD: Datatypes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
 fn main() {
-    let mut x = 5; // mutable variable
-    let y = 8; // immutable variable
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("Guess: {}", guess);
 
-    println!("The value of x is: {x}"); // before updating x
-    x = 6;
-    println!("The value of x is: {x}"); // after updating x
+    let x: i32 = 5; // integer
+    let y: f32 = 3.0; // floating number
+    let z: bool = true; // boolean
+    let a: char = 'a'; // character
+    let b: [i32; 5] = [1, 2, 3, 4, 5]; // array - [type; size]
+    let c: (i32, f64, u8) = (500, 6.4, 1); // tuple
+    let (d, e, f) = c; // destructuring tuple
+    let g = c.0; // access tuple element
+    let h = c.1; // access tuple element
 
-    println!("The value of y is: {y}");
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    println!("Months: {:?}", months);
 
-    {
-        let x = x * 2; // shadowing && local scope
-        println!("The value of x in the inner scope is: {x}"); // access the inner scope
-    }
-
-    println!("The value of x is: {x}"); // access the outer scope
-
-    let x = x * 2; // shadowing
-    println!("The value of x is: {x}");
+    println!("x: {}", x);
+    println!("y: {}", y);
+    println!("z: {}", z);
+    println!("a: {}", a);
+    println!("b: {:?}", b);
+    println!("c: {:?}", c);
+    println!("d: {}", d);
+    println!("e: {}", e);
+    println!("f: {}", f);
+    println!("g: {}", g);
+    println!("h: {}", h);
 }
