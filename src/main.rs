2484eb85576f93415fc182a2c9e9053ee40b74ea
rahulcodepython <rahulcodepython@gmail.com>
2024-10-18 02:39:58 +0000

    ADD: Ownership Exchange

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
 fn main() {
-    let a = 5; // owned by main function
-    {
-        let b = 10; // owned by inner block
-        println!("a: {}, b: {}", a, b);
-    }
-    println!("a: {}", a);
+    let mut a = 1;
 
-    let x = 7; // owned by main function
-    let y = 8; // owned by main function
-    let z = sum(x, y); // z is owned by main function
-                       // x and y are borrowed by sum function, so they are not accessible here
-    println!("sum: {}", z);
+    a = takes_and_return_ownership(a); // a is moved to takes_and_return_ownership. Now it is owned by takes_and_return_ownership
+    println!("{}", a); // This will print 1 because a is moved to takes_and_return_ownership and then returned back to main
 
-    copy();
+    takes_reference(&a); // a is borrowed by takes_reference. It is still owned by main
+
+    takes_ownership(a); // a is moved to takes_ownership. Now it is owned by takes_ownership
+                        // println!("{}", a); // This will give an error because a is moved to takes_ownership
 }
 
-fn sum(a: i32, b: i32) -> i32 {
-    a + b // return value
+fn takes_and_return_ownership(a: i32) -> i32 {
+    println!("{}", a);
+    a
 }
 
-fn copy() {
-    let s1 = String::from("hello");
-    let s2 = s1; // s1 is moved to s2
-                 // println!("s1: {}", s1); // error: value borrowed here after move
-    println!("s2: {}", s2);
-    let s3 = s2.clone(); // s2 is cloned to s3
-    println!("s2: {}, s3: {}", s2, s3);
+fn takes_reference(a: &i32) {
+    println!("{}", a);
+}
+
+fn takes_ownership(a: i32) {
+    println!("{}", a);
 }
