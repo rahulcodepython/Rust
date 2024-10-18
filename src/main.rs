a6d87a55cd3ed0264e0d879a6f5b581db6e4b823
rahulcodepython <rahulcodepython@gmail.com>
2024-10-18 02:57:30 +0000

    ADD: Reference & Mutable Reference

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
     let mut s = String::from("hello");
 
-    change(&mut s);
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s;
 
-    println!("{}", s);
-}
+    // There may have multiple reference variables of a variable at a same time in a scope. But, there can be only one mutable reference variable of a variable at a same time in a scope without
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    // When I am taking a mutable reference of a variable, I can't take any other reference of that variable in the same scope. This is because, mutable reference can change the value of the variable, so it is not safe to have other reference of that variable in the same scope.
+
+    // If we do this, we will get a compile time error. Because mutable reference may change the value of the variable which doesn't reflect the change to the other reference variables. So, it is not safe.
+
+    println!("{}, {}, and {}", r1, r2, r3);
 }
