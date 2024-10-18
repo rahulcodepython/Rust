6a5eb7f801112012fb0cb5ec3caafcfbc99c8143
rahulcodepython <rahulcodepython@gmail.com>
2024-10-18 02:43:50 +0000

    MODIFY: String Reference Exchange

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
 fn main() {
-    let mut a = 1;
+    let s1 = String::from("hello");
 
-    a = takes_and_return_ownership(a); // a is moved to takes_and_return_ownership. Now it is owned by takes_and_return_ownership
-    println!("{}", a); // This will print 1 because a is moved to takes_and_return_ownership and then returned back to main
+    let (s2, len) = calculate_length(s1);
 
-    takes_reference(&a); // a is borrowed by takes_reference. It is still owned by main
-
-    takes_ownership(a); // a is moved to takes_ownership. Now it is owned by takes_ownership
-                        // println!("{}", a); // This will give an error because a is moved to takes_ownership
+    println!("The length of '{s2}' is {len}.");
 }
 
-fn takes_and_return_ownership(a: i32) -> i32 {
-    println!("{}", a);
-    a
-}
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
 
-fn takes_reference(a: &i32) {
-    println!("{}", a);
-}
-
-fn takes_ownership(a: i32) {
-    println!("{}", a);
+    (s, length)
 }
